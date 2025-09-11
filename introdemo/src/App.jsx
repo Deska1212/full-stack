151b43619375d5d9da4fbe7d49a63f003b83128b
Daniel Findell <daniel.fin@outlook.com>
2025-09-11 21:32:31 +1000

    Finished part 1

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
 
 const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+
+  const [votes, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0})
 
 
   const handleGood = () => {
@@ -19,9 +34,23 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const displayAnecdote = () => {
+    setSelected((selected + 1) % anecdotes.length)
+  }
+
+  const handleUpvote = () => {
+    const copyOfVotes = {...votes}
+    copyOfVotes[selected] += 1
+    setVotes(copyOfVotes)
+  }
+
   const total = good + neutral + bad
   const average = total === 0 ? 0 : (good-bad) / total
   const positivePercentage = total === 0 ? 0 : (good / total) * 100
+
+  const votesArray = Object.values(votes)
+  const maxVotes = Math.max(...votesArray)
+  const highestVotesIndex = votesArray.indexOf(maxVotes)
 
   return (
     <div>
@@ -47,9 +76,14 @@ const App = () => {
         ) : (<p>No feedback given</p>)
 
       )}
-
-
-      
+        <hr/>
+        <Button onClick={displayAnecdote} text="Next Anecdote"/>
+        <Button onClick={handleUpvote} text="Upvote"/>
+        <p>{anecdotes[selected]} has {votes[selected]} votes</p>
+        
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[highestVotesIndex]}</p>
+        
     </div>
   )
 }
